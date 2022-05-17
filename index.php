eb58cd40b9a9049e4c5dc920a5203266564d10b1
F1GUR <92974233+F1GUR@users.noreply.github.com>
2022-05-17 17:48:37 +0200

    Display Services and Prices from Database

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
-echo "Hello World";
+$db = new mysqli("localhost", "root", "", "mech");
+$q = $db->prepare("SELECT * FROM offer");
+if($q->execute()) {
+    $result =$q->get_result();
+    while($row = $result->fetch_assoc()) {
+        $id = $row['id'];
+        $Nazwa = $row['Nazwa'];
+        $Cena = $row['Cena'];
+
+        echo "Usługa: $Nazwa $Cena<br>";
+
+
+    }
+
+} else {
+   
+ die("Błąd pobierania informacji z bazy");
+
+    
+}
 
 ?>
